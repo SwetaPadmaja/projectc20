25b135e040f0fb759fe24615de74c284836a5b09
swetaprogrammer <71880635+swetaprogrammer@users.noreply.github.com>
2020-10-28 23:53:12 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -20,8 +20,8 @@ function setup() {
 
 
   
-  	wall=createSprite(1500,200, 60, height/2)
-  	wall.shapeColor=color(80,80,80)
+  	wall=createSprite(1300,200, 60, height/2);
+  	wall.shapeColor=color(80,80,80);
 }
 
 
